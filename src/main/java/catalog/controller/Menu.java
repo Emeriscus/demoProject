f42590af7a481175648ff79db9c470f91ab58a2a
Emeriscus <kimre88@gmail.com>
2022-04-12 13:44:13 +0200

    update v2.61 Menu metódusok kiszervezése egy külön osztályba (MenuItems)

--- src/main/java/catalog/controller/Menu.java
+++ src/main/java/catalog/controller/Menu.java
@@ -15,9 +15,11 @@ public class Menu {
     private Scanner scanner = new Scanner(System.in);
     private int choice = 0;
     private LibraryService libraryService;
+    private MenuItems menuItems;
 
     public Menu(LibraryService libraryService) {
         this.libraryService = libraryService;
+        menuItems = new MenuItems(libraryService);
     }
 
     public void runMenu() {
@@ -25,28 +27,36 @@ public class Menu {
             printMenu();
             switch (choice) {
                 case 1:
-                    runMenuItemOne();
+                    menuItems.runMenuItemOne();
+                    choice = 0;
                     break;
                 case 2:
-                    runMenuItemTwo();
+                    menuItems.runMenuItemTwo();
+                    choice = 0;
                     break;
                 case 3:
-                    runMenuItemThree();
+                    menuItems.runMenuItemThree();
+                    choice = 0;
                     break;
                 case 4:
-                    runMenuItemFour();
+                    menuItems.runMenuItemFour();
+                    choice = 0;
                     break;
                 case 5:
-                    runMenuItemFive();
+                    menuItems.runMenuItemFive();
+                    choice = 0;
                     break;
                 case 6:
-                    runMenuItemSix();
+                    menuItems.runMenuItemSix();
+                    choice = 0;
                     break;
                 case 7:
-                    runMenuItemSeven();
+                    menuItems.runMenuItemSeven();
+                    choice = 0;
                     break;
                 case 8:
-                    runMenuItemEight();
+                    menuItems.runMenuItemEight();
+                    choice = 0;
                     break;
                 case 9:
                 default:
@@ -77,179 +87,4 @@ public class Menu {
             scanner.nextLine();
         }
     }
-
-    private void runMenuItemOne() {
-        System.out.println("What type of library item would you like add? (Book, Audio) ");
-        String input = scanner.nextLine();
-        if (Validators.isBlank(input)) {
-            System.out.println("The type cannot be empty! Please press Enter and try again!");
-            scanner.nextLine();
-            return;
-        }
-        if (!input.equals("Book") && !input.equals("Audio")) {
-            System.out.println("The type must be Book or Audio! Please press Enter and try again!");
-            scanner.nextLine();
-        }
-        addLibraryItem(input);
-    }
-
-    private void runMenuItemTwo() {
-        System.out.print("Please type the title to search for: ");
-        System.out.println();
-        String input = scanner.nextLine();
-        if (libraryService.getLibraryItemByTitle(input).isPresent()) {
-            System.out.println(libraryService.getLibraryItemByTitle(input).get());
-        }
-        choice = 0;
-        System.out.println();
-        System.out.println("Continue with Enter");
-        scanner.nextLine();
-    }
-
-    private void runMenuItemThree() {
-        System.out.print("Please type the ID to search for: ");
-        System.out.println();
-        String input = scanner.nextLine();
-        if (!Validators.isNumber(input)) {
-            System.out.println("Not a number! Please press Enter and choose a number!");
-        }
-        if (Validators.isNumber(input) && libraryService.getLibraryItemById(Integer.parseInt(input)).isPresent()) {
-            System.out.println(libraryService.getLibraryItemById(Integer.parseInt(input)).get());
-        }
-        choice = 0;
-        System.out.println();
-        System.out.println("Continue with Enter");
-        scanner.nextLine();
-    }
-
-    private void runMenuItemFour() {
-        System.out.print("Please type the title: ");
-        System.out.println();
-        String input = scanner.nextLine();
-        if (libraryService.getLibraryItemByTitle(input).isPresent()) {
-            libraryService.deleteLibraryItemByTitle(input);
-            System.out.println();
-            System.out.println("The delete was successful");
-        }
-        choice = 0;
-        System.out.println("Continue with Enter");
-        scanner.nextLine();
-    }
-
-    private void runMenuItemFive() {
-        System.out.print("Please type the ID: ");
-        System.out.println();
-        String input = scanner.nextLine();
-        if (!Validators.isNumber(input)) {
-            System.out.println("Not a number! Please press Enter and try again!");
-        }
-        if (Validators.isNumber(input) && libraryService.getLibraryItemById(Integer.parseInt(input)).isPresent()) {
-            libraryService.deleteLibraryItemById(Integer.parseInt(input));
-            System.out.println("The delete was successful");
-            System.out.println("Continue with Enter");
-            scanner.nextLine();
-        } else {
-            choice = 0;
-            System.out.println();
-            System.out.println("Continue with Enter");
-            scanner.nextLine();
-        }
-    }
-
-    private void runMenuItemSix() {
-        System.out.println();
-        System.out.println(libraryService.getAllLibraryItem());
-        choice = 0;
-        System.out.println("Continue with Enter");
-        scanner.nextLine();
-    }
-
-    private void runMenuItemSeven() {
-        System.out.print("Please type the title: ");
-        System.out.println();
-        System.out.println(libraryService.getAllLibraryItemByTitleFragment(scanner.nextLine()));
-        choice = 0;
-        System.out.println("Continue with Enter");
-        scanner.nextLine();
-    }
-
-    private void runMenuItemEight() {
-        System.out.println("Please type the title: ");
-        String input = scanner.nextLine();
-        if (libraryService.getLibraryItemByTitle(input).isPresent() &&
-                libraryService.hasAvailableLibraryItemQuantitybyId(libraryService.getLibraryItemIdByTitle(input))) {
-            libraryService.borrowLibraryItemByTitle(input);
-            System.out.println();
-            System.out.println("The borrow was successful");
-            System.out.println("Continue with Enter");
-            scanner.nextLine();
-        } else {
-            choice = 0;
-            System.out.println(("There is not available item!"));
-            System.out.println("Continue with Enter");
-            scanner.nextLine();
-        }
-    }
-
-    private void addLibraryItem(String itemType) {
-        if (itemType.equals("Book")) {
-            addBook();
-        }
-        if (itemType.equals("Audio")) {
-            addAudio();
-        }
-    }
-
-    private void addBook() {
-        System.out.println("title ");
-        String title = scanner.nextLine();
-        if (Validators.isBlank(title)) {
-            throw new IllegalArgumentException("The title cannot be empty");
-        }
-        System.out.println("authors separated ', ' ");
-        List<String> authors = new ArrayList<>(StringListConverters.stringToList(scanner.nextLine()));
-        System.out.println("year ");
-        int yearOfProduction = scanner.nextInt();
-        if (!Validators.isValidYear(yearOfProduction)) {
-            throw new IllegalArgumentException("The year must be between 1000 and the current year");
-        }
-        System.out.println("quantity ");
-        int quantity = scanner.nextInt();
-        if (!Validators.isValidQuantity(quantity)) {
-            throw new IllegalArgumentException("The quantity must not above 0");
-        }
-        scanner.nextLine();
-        Book book = new Book(title, authors, yearOfProduction, quantity);
-        libraryService.addLibraryItem(book);
-        System.out.println("Add item was successful!");
-        System.out.println("Continue with Enter");
-        scanner.nextLine();
-    }
-
-    private void addAudio() {
-        System.out.println("title ");
-        String title = scanner.nextLine();
-        System.out.println("performers separated ', ' ");
-        List<String> performers = new ArrayList<>(StringListConverters.stringToList(scanner.nextLine()));
-        System.out.println("composers separated ', ' ");
-        List<String> composers = new ArrayList<>(StringListConverters.stringToList(scanner.nextLine()));
-        System.out.println("year ");
-        int yearOfProduction = scanner.nextInt();
-        if (!Validators.isValidYear(yearOfProduction)) {
-            throw new IllegalArgumentException("The year must be between 1000 and the current year");
-        }
-        System.out.println("quantity ");
-        int quantity = scanner.nextInt();
-        if (!Validators.isValidQuantity(quantity)) {
-            throw new IllegalArgumentException("The quantity must not above 0");
-        }
-        scanner.nextLine();
-        Audio audio = new Audio(title, performers, composers, yearOfProduction, quantity);
-        libraryService.addLibraryItem(audio);
-        System.out.println("Add item was successful!");
-        System.out.println("Continue with Enter");
-        scanner.nextLine();
-    }
-
-
 }
